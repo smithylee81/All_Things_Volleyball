488f02f70ebe02b916cce7fa5fd164ed959d331d
smithylee81 <smithylee81@yahoo.co.uk>
2022-06-13 14:35:35 +0000

    Product admin - add widget for image fix

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-#from .widgets import CustomClearableFileInput
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -9,9 +9,7 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    # image = forms.ImageField(label='Image',
-    #                          required=False,
-    #                          widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
