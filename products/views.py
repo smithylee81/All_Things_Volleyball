f62d38af489801bbf70a3dc852c12593674c2782
smithylee81 <smithylee81@yahoo.co.uk>
2022-06-13 13:18:21 +0000

    Finish add product functionality

--- products/views.py
+++ products/views.py
@@ -79,9 +79,9 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
+    # if not request.user.is_superuser:
+    #     messages.error(request, 'Sorry, only store owners can do that.')
+    #     return redirect(reverse('home'))
 
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
