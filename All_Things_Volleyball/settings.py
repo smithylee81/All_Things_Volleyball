128375b46777e912d24f9d1e72fde6da9e4d8de8
smithylee81 <smithylee81@yahoo.co.uk>
2022-07-25 22:01:37 +0000

    Final commit, set debug to False

--- All_Things_Volleyball/settings.py
+++ All_Things_Volleyball/settings.py
@@ -35,7 +35,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['all-things-volleyball.herokuapp.com', 'localhost']
 
